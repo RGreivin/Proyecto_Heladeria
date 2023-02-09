f557f6e82d2eadc36703346d212cba957cabbd75
Greivin Rojas <andreyrojasc@hotmail.com>
2023-02-08 23:14:16 -0600

    Inicio listo

--- Proyecto_Heladeria/PL/Pantalla/Inicio.Designer.cs
+++ Proyecto_Heladeria/PL/Pantalla/Inicio.Designer.cs
@@ -29,20 +29,92 @@ namespace PL.Pantalla
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Lbl_Progress = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(356, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(42, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(278, 217);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(42, 274);
+            this.progressBar1.Maximum = 101;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(278, 28);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Lbl_Progress
+            // 
+            this.Lbl_Progress.AutoSize = true;
+            this.Lbl_Progress.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Lbl_Progress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Progress.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Progress.ForeColor = System.Drawing.Color.White;
+            this.Lbl_Progress.Location = new System.Drawing.Point(151, 318);
+            this.Lbl_Progress.Name = "Lbl_Progress";
+            this.Lbl_Progress.Size = new System.Drawing.Size(44, 29);
+            this.Lbl_Progress.TabIndex = 3;
+            this.Lbl_Progress.Text = "0%";
             // 
             // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.ClientSize = new System.Drawing.Size(356, 365);
+            this.Controls.Add(this.Lbl_Progress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inicio";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label Lbl_Progress;
     }
 }
--- Proyecto_Heladeria/PL/Pantalla/Inicio.cs
+++ Proyecto_Heladeria/PL/Pantalla/Inicio.cs
@@ -16,5 +16,27 @@ namespace PL.Pantalla
         {
             InitializeComponent();
         }
-    }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(4);
+            Opacity -= 0.020;
+            Lbl_Progress.Text = progressBar1.Value.ToString() + "%";
+            if (Opacity == 0.0)
+            {
+                timer1.Stop();
+                this.Hide();
+                Pantalla.Login log = new Pantalla.Login();
+                log.ShowDialog();
+            }
+            //    Lbl_Progress.Text = progressBar1.Value.ToString() + "%";
+            //    if (progressBar1.Value == progressBar1.Maximum)
+            //    {
+            //        timer1.Stop();
+            //        this.Hide();
+            //        Pantalla.Login  log= new Pantalla.Login();
+            //        log.ShowDialog();
+            //    }
+        }   
+     }
 }
