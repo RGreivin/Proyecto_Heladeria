9a1c06b4f2673d9acbf52eb32386637aa1fe2541
Greivin Rojas <andreyrojasc@hotmail.com>
2023-02-09 11:48:06 -0600

    cambiios en inicio

--- Proyecto_Heladeria/PL/Pantalla/Inicio.Designer.cs
+++ Proyecto_Heladeria/PL/Pantalla/Inicio.Designer.cs
@@ -36,6 +36,7 @@ namespace PL.Pantalla
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.Lbl_Progress = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -43,7 +44,6 @@ namespace PL.Pantalla
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(356, 365);
@@ -69,24 +69,33 @@ namespace PL.Pantalla
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(42, 274);
-            this.progressBar1.Maximum = 101;
+            this.progressBar1.Location = new System.Drawing.Point(42, 260);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(278, 28);
+            this.progressBar1.Size = new System.Drawing.Size(278, 29);
             this.progressBar1.TabIndex = 2;
             // 
             // Lbl_Progress
             // 
             this.Lbl_Progress.AutoSize = true;
-            this.Lbl_Progress.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.Lbl_Progress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl_Progress.BackColor = System.Drawing.Color.Transparent;
             this.Lbl_Progress.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Progress.ForeColor = System.Drawing.Color.White;
-            this.Lbl_Progress.Location = new System.Drawing.Point(151, 318);
+            this.Lbl_Progress.ForeColor = System.Drawing.Color.Black;
+            this.Lbl_Progress.Location = new System.Drawing.Point(146, 298);
             this.Lbl_Progress.Name = "Lbl_Progress";
-            this.Lbl_Progress.Size = new System.Drawing.Size(44, 29);
+            this.Lbl_Progress.Size = new System.Drawing.Size(42, 27);
             this.Lbl_Progress.TabIndex = 3;
             this.Lbl_Progress.Text = "0%";
+            this.Lbl_Progress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Loading...";
             // 
             // Inicio
             // 
@@ -94,6 +103,7 @@ namespace PL.Pantalla
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(356, 365);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Lbl_Progress);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox2);
@@ -116,5 +126,6 @@ namespace PL.Pantalla
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label Lbl_Progress;
+        private System.Windows.Forms.Label label1;
     }
 }
